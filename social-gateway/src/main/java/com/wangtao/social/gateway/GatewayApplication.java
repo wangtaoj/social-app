0bef342afb9d1730fd8fad50031e4bdd6f6d067a
wangtao <wangtao970503@foxmail.com>
2023-09-17 00:49:28 +0800

    引入网关

---
+++ social-gateway/src/main/java/com/wangtao/social/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.wangtao.social.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangtao
+ * Created at 2023-09-16
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
