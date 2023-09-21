def6a9c575ee91250b73bc89122ac6d876d41f95
wangtao <wangtao970503@foxmail.com>
2023-09-21 23:31:58 +0800

    集成knife4j+springdoc，并且支持javadoc格式生成文档

---
+++ social-common/social-common-core/src/main/java/com/wangtao/social/common/core/config/SpringDocConfig.java
@@ -0,0 +1,30 @@
+package com.wangtao.social.common.core.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author wangtao
+ * Created at 2023-09-10
+ */
+@Configuration
+public class SpringDocConfig {
+
+    @ConditionalOnMissingBean
+    @Bean
+    public OpenAPI openAPI(@Value("${springdoc.info.desp}")String desp) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Social APP API")
+                        .version("1.0")
+                        .description(desp)
+                        .termsOfService("wangtaoj.github.io")
+                        .license(new License().name("Apache 2.0").url("wangtaoj.github.io"))
+                );
+    }
+}
