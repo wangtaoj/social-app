6a22246692ab5245854226180700b66369a58779
wangtao <wangtao970503@foxmail.com>
2023-09-23 21:43:05 +0800

    网关引入redis、core模块

--- social-gateway/src/main/java/com/wangtao/social/gateway/GatewayApplication.java
+++ social-gateway/src/main/java/com/wangtao/social/gateway/GatewayApplication.java
@@ -1,5 +1,6 @@
 package com.wangtao.social.gateway;
 
+import com.wangtao.social.common.core.config.SocialAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author wangtao
  * Created at 2023-09-16
  */
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {SocialAutoConfiguration.class},
+        scanBasePackages = {
+            "com.wangtao.social.gateway",
+            "com.wangtao.social.common.redis"
+        }
+)
 public class GatewayApplication {
 
     public static void main(String[] args) {
