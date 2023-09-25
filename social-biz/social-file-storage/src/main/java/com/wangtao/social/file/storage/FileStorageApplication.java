0398ea543637d0a2db2552a89ce227d574cd869c
wangtao <wangtao970503@foxmail.com>
2023-09-25 19:14:47 +0800

    文件存储服务模块初始化

---
+++ social-biz/social-file-storage/src/main/java/com/wangtao/social/file/storage/FileStorageApplication.java
@@ -0,0 +1,16 @@
+package com.wangtao.social.file.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangtao
+ * Created at 2023-09-25
+ */
+@SpringBootApplication
+public class FileStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileStorageApplication.class, args);
+    }
+}
