98c2322bd84492d04b4bd043bb1d26405f0dc5f1
wangtao <wangtao970503@foxmail.com>
2023-09-26 19:34:06 +0800

    广场服务模块搭建

---
+++ social-biz/social-square/src/main/java/com/wangtao/social/square/SocialSquareApplication.java
@@ -0,0 +1,16 @@
+package com.wangtao.social.square;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangtao
+ * Created at 2023-09-26
+ */
+@SpringBootApplication
+public class SocialSquareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialSquareApplication.class, args);
+    }
+}
