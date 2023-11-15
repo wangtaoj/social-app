d08d609adac9c2933bdff74d8cb60f11ef0ab1f2
wangtao <wangtao970503@foxmail.com>
2023-11-15 20:05:05 +0800

    优化工具类

--- social-common/social-common-core/src/main/java/com/wangtao/social/common/core/util/IpUtils.java
+++ social-common/social-common-core/src/main/java/com/wangtao/social/common/core/util/IpUtils.java
@@ -1,14 +1,18 @@
 package com.wangtao.social.common.core.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 
-import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Objects;
 
 /**
  * @author wangtao
  * Created at 2023-09-23
  */
+@Slf4j
 public final class IpUtils {
 
     private static final String X_REAL_IP = "X-Real-IP";
@@ -25,11 +29,21 @@ public final class IpUtils {
         if (StringUtils.isNotBlank(realIp)) {
             return realIp;
         }
+        String ip = null;
         if (request.getRemoteAddress() != null) {
-            if (request.getRemoteAddress().getAddress() instanceof Inet4Address) {
-                return request.getRemoteAddress().getHostString();
+            ip = request.getRemoteAddress().getAddress().getHostAddress();
+        }
+        if (isLoopBackAddress(ip)) {
+            try {
+                ip = InetAddress.getLocalHost().getHostAddress();
+            } catch (UnknownHostException e) {
+                log.error("获取本地ip地址发生异常", e);
             }
         }
-        return request.getURI().getHost();
+        return ip;
+    }
+
+    private static boolean isLoopBackAddress(String address) {
+        return Objects.equals("127.0.0.1", address) || Objects.equals("0:0:0:0:0:0:0:1", address);
     }
 }
