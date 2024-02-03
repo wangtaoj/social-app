d015d9449eadb1f9f960d7cb2a6683ba7f36844f
wangtao <wangtao970503@foxmail.com>
2024-02-03 18:44:42 +0800

    修改es jackson配置, 不再使用容器中注入的ObjectMapper

--- social-common/social-common-es/src/main/java/com/wangtao/social/common/es/config/ElasticsearchConfig.java
+++ social-common/social-common-es/src/main/java/com/wangtao/social/common/es/config/ElasticsearchConfig.java
@@ -4,12 +4,21 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.wangtao.social.common.core.util.JavaTimeModuleUtils;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
 
 /**
  * @author wangtao
@@ -18,6 +27,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ElasticsearchConfig {
 
+    private static final String STANDARD_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
     @Value("${es.host}")
     private String host;
 
@@ -25,13 +36,46 @@ public class ElasticsearchConfig {
     private int port;
 
     @Bean
-    public ElasticsearchClient elasticsearchClient(ObjectMapper objectMapper) {
+    public ElasticsearchClient elasticsearchClient() {
         RestClient restClient = RestClient.builder(
                 new HttpHost(host, port)).build();
 
+        /*
+         * 7.x版本, JacksonJsonpMapper构造方法会修改传入的ObjectMapper属性
+         * 切记不要使用Spring容器中的ObjectMapper，否则会影响SpringMVC JSON序列化行为
+         */
         ElasticsearchTransport transport = new RestClientTransport(
-                restClient, new JacksonJsonpMapper(objectMapper));
+                restClient, new JacksonJsonpMapper(createObjectMapper()));
 
         return new ElasticsearchClient(transport);
+    }
+
+    private ObjectMapper createObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        //设置java.util.Date时间类的序列化以及反序列化的格式
+        objectMapper.setDateFormat(new SimpleDateFormat(STANDARD_PATTERN));
+
+        // 初始化JavaTimeModule
+        JavaTimeModule javaTimeModule = JavaTimeModuleUtils.create();
+
+        // 注册模块
+        objectMapper.registerModule(javaTimeModule);
+
+        // 注册JDK新增的一些类型, 比如Optional
+        objectMapper.registerModule(new Jdk8Module());
+
+        // 包含所有字段
+        objectMapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
+
+        // 在序列化一个空对象时时不抛出异常
+        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+
+        // 忽略反序列化时在json字符串中存在, 但在java对象中不存在的属性
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        // BigDecimal.toPlainString(), 这样不会有科学计数法
+        objectMapper.enable(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN);
+        return objectMapper;
     }
 }
